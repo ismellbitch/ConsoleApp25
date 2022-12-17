0d76d15eda51ba19e60885d91521cbd3750b3d21
Артём Коваленко <ak7527203@gmail.com>
2022-12-17 19:06:28 +0200

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp25;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите сторону a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите сторону b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите сторону c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        TriangleType type = new(a, b, c);
+
+        string res = type.GetTriangleTyple();
+        Console.WriteLine(res);
+    }
+}
---
+++ ConsoleApp25/TriangleType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class TriangleType
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public TriangleType(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double A 
+        {
+            get { return a; }
+            set { if (a > 0) a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { if (b > 0) b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { if (c > 0) c = value; }
+        }
+
+        public string GetTriangleTyple()
+        {
+            if (A == B && A == C) return "Треугольник равносторонний";
+            else if (A == Math.Sqrt(B * B + C * C) || B == Math.Sqrt(A * A + C * C) || C == Math.Sqrt(A * A + B * B)) return "Треугольник прямоугольный";
+            else return "Треугольник простой";
+        }
+    }
+}
